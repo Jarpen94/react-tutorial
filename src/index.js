2b5aa1a979c489345cdc61dd4031b528dc7469f2
Jarpen94 <kwiatkowskijarek@o2.pl>
2018-11-24 23:12:24 +0100

    defined jumpTo method in Game component

--- src/index.js
+++ src/index.js
@@ -60,6 +60,7 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null)
             }],
+            stepNumber: 0,
             xIsNext: true,
         }
     }
@@ -79,6 +80,13 @@ class Game extends React.Component {
                 squares: squares
             }]),
             xIsNext: !this.state.xIsNext
+        })
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
         })
     }
 
